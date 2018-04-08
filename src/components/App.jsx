b084ff3e9bc5f3368acd0d38c36a384559f0fc6c
Sam Wellander <samwellander@gmail.com>
2018-04-08 14:22:32 -0700

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+import Feed from './Feed.jsx';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Feed />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Post(props) {
+    return (
+        <div>
+            <h2>{props.votes} | {props.title}</h2>
+            <h4><em>User: {props.userName}</em></h4>
+            <p>{props.postBody}</p>
+            <hr/>
+        </div>
+    )
+}
+
+Post.propTypes = {
+    title: PropTypes.string,
+    userName: PropTypes.string,
+    postBody: PropTypes.string
+}
+
+export default Post;
